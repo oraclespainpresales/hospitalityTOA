4715584eb9ccba1c3aa431498e60c6699c716e8e
carlos_picazo_sordo <carlos.picazo.sordo@oracle.com>
2017-08-10 15:16:41 +0200

    change ports

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ var path = require('path');
 module.exports = {
   server: {
     port: 19991,
-    securePort: 19992,                                 
-    cookieSecret: 'etadirect-tools',
+    securePort: 19992,
+    cookieSecret: 'etadirect-tools', 
     distFolder:  __dirname,
     staticUrl: __dirname,
   	ip: '0.0.0.0',
